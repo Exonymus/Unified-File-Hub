9e0c33b78293c41e90918218a237f85e881c6305
Igor Aralin <exonymus@icloud.com>
2024-05-14 16:44:02 +0300

    Repository CleanUp

--- Web/src/main/resources/static/js/sort_table_worker.js
+++
@@ -1,6 +0,0 @@
-import {getSort} from "./controllers/sort_table_controller.js";
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.table_sort thead').forEach(tableTH =>
-        tableTH.addEventListener('click', () => getSort(event)));
-});
--- Web/src/main/resources/static/js/views/sort_table_view.js
+++
@@ -1,9 +0,0 @@
-export function appendSortRows ({ target }, order, index, comparator) {
-    for (const tBody of target.closest('table').tBodies)
-        tBody.append(...[...tBody.rows].sort(comparator(index, order)));
-}
-
-export function cellUpdate({ target }) {
-    for (const cell of target.parentNode.cells)
-        cell.classList.toggle('sorted', cell === target);
-}
